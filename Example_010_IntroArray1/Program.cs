41ea5afdd02b7443bf823f5da5b32189b78790c3
Artem_Sidorenko <artem70947@gmail.com>
2023-04-18 14:50:02 +0300

    Решение задач с массивом с помощью Метода(Функции) продолжение

---
+++ Example_010_IntroArray1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Решение задачи с массивом с помощью Метода(Функции) продолжение
+
+
+// Индекс массива   0  1   2   3   4  5   6   7
+int[] array =     {11, 2, 33, 18, 15, 6, 17, 28 };
+
+int n = array.Length;
+
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+// index = index + 1;
+    index++;
+}
